8ba938a730464ada414fff80b8c72600dc67f4d0
felipe <felipe.rios@mail.udp.cl>
2014-09-08 18:05:16 -0300

    validador arreglado
    asas

--- proyecto_cafe/principal/models.py
+++ proyecto_cafe/principal/models.py
@@ -2,17 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 import re
+from datetime import date
 # Create your models here.
 def validate_nombre(value):
     if len(value) < 3 :
         raise ValidationError('%s ->Nombre muy corto' % value)
-    if  value.find('[^A-Za-z]'):
+    if  re.search(r"[0-9]", value) >= 0:
     	raise ValidationError('%s ->Se ingreso un caracter incorrecto' % value)
 class Trabajador(models.Model):
-	#GastoAcumulado = models.IntegerField(null=True)
+	CafesComprados = models.IntegerField(null=True)
 	Nombre = models.CharField(max_length=20,validators=[validate_nombre])
 	Apellido = models.CharField(max_length=20,validators=[validate_nombre])
 	Direccion = models.CharField(max_length=50)
+	Cant_CafesConsumidos = models.IntegerField(null=True)
+	Cant_CafesComprados = models.IntegerField(null=True)
+    #FechaUltimaCompra = models.DateField(_("Date"), auto_now_add=True)
+
 
 	def __unicode__(self):
 		return (self.Nombre)
