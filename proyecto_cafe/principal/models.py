0c46d5267e0661a76a67d5bd1f4165c64e1a8f97
felipe <felipe.rios@mail.udp.cl>
2014-09-08 17:25:59 -0300

    validados fin

--- proyecto_cafe/principal/models.py
+++ proyecto_cafe/principal/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+import re
 # Create your models here.
 def validate_nombre(value):
     if len(value) < 3 :
         raise ValidationError('%s ->Nombre muy corto' % value)
-    #if  re.match("[0-9]", value):
-    	#raise ValidationError('%s ->No se aceptan numeros' % value)
+    if  value.find('[^A-Za-z]'):
+    	raise ValidationError('%s ->Se ingreso un caracter incorrecto' % value)
 class Trabajador(models.Model):
 	#GastoAcumulado = models.IntegerField(null=True)
 	Nombre = models.CharField(max_length=20,validators=[validate_nombre])
